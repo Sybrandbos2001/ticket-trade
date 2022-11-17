eec069f24f21a643740f7223eafcbe62d3670234
Sybrand Bos <snjw.bos@student.avans.nl>
2022-11-17 15:58:35 +0100

    concert detail page dynamic

--- apps/ticket-trade/src/app/pages/concert/concert-detail/concert-detail.component.ts
+++ apps/ticket-trade/src/app/pages/concert/concert-detail/concert-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConcertService } from '@ticket-trade/shared/services';
+import { Concert } from '@ticket-trade/shared/domains';
 
 @Component({
   selector: 'ticket-trade-concert-detail',
@@ -7,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConcertDetailComponent implements OnInit {
 
-  constructor() { }
+  id: string | null | undefined;
+  concert: Concert | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private concertService: ConcertService) { 
+  }
 
   ngOnInit(): void {
-    
+    this.route.paramMap.subscribe((params) => {
+      this.id = params.get("id");
+      if (this.id) {
+          // Bestaand concert
+          this.concert = this.concertService.getConcertById(this.id);
+      } else{
+        this.router.navigateByUrl('concerten');
+      }
+  });
   }
 
 }
--- libs/shared/services/src/lib/concert.service.ts
+++ libs/shared/services/src/lib/concert.service.ts
@@ -8,10 +8,10 @@ export class ConcertService {
     //Seed data
     this.concerts = [
       {
-        id: 1,
+        id: '1',
         name: 'Coldplay - Music Of The Spheres World Tour',
         description: 'De Music of the Spheres World Tour is de achtste concerttournee die momenteel wordt ondernomen door de Britse rockband Coldplay. Het werd aangekondigd op 14 oktober 2021 ter ondersteuning van hun negende studioalbum Music of the Spheres en markeerde een terugkeer naar live optreden na de COVID-19-pandemie.',
-        startDateTime: new Date('Sun Jul 16 2023 20:00:00'),
+        startDateTime: new Date('Sun Jul 16 2023 20:30:00'),
         endDateTime: new Date('Sun Jul 16 2023 23:00:00'),
         location: 'Johan Cruijff ArenA, Amsterdam, Nederland',
         artist: 'Coldplay',
@@ -19,7 +19,7 @@ export class ConcertService {
         amountTickets: 2000,
       },
       {
-        id: 2,
+        id: '2',
         name: 'The Chainsmokers',
         description: 'The Chainsmokers is een Amerikaans dj-duo bestaande uit Andrew Taggart en Alex Pall. Tevens heeft in 2017 ook de drummer Matt McGuire zich bij de band gevoegd.',
         startDateTime: new Date('Wen Nov 16 2022 20:00:00'),
@@ -30,7 +30,7 @@ export class ConcertService {
         amountTickets: 2500,
       },
       {
-        id: 3,
+        id: '3',
         name: 'Red Hot Chili Peppers- 20th Anniversary European Tour 2022',
         description:'Red Hot Chilli Peppers is een Keltische rockband uit Schotland. Gevormd in 2002, werden ze internationaal populair in 2007 na het winnen van de BBC-talentenshow When Will I Be Famous?',
         startDateTime: new Date('Wen Nov 23 2022 20:00:00'),
@@ -47,7 +47,7 @@ export class ConcertService {
     return this.concerts;
   }
 
-  public getConcertById(Id: number): Concert {
+  public getConcertById(Id: string): Concert {
     return this.concerts.filter((concert: Concert) => concert.id === Id)[0];
   }
 }
