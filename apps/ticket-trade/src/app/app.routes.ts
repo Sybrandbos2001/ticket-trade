9ce387ddfd0f8a448651ef5665b2004d22d23b3a
Sybrand <snjw.bos@student.avans.nl>
2022-11-17 12:56:29 +0100

    Added concert detail page and routing

--- apps/ticket-trade/src/app/app.routes.ts
+++ apps/ticket-trade/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Route } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { ConcertListComponent } from './pages/concert/concert-list/concert-list.component';
+import { ConcertDetailComponent } from './pages/concert/concert-detail/concert-detail.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: LandingComponent },
+  { path: 'concerten/:id', component: ConcertDetailComponent },
   { path: 'concerten', component: ConcertListComponent },
 ];
---
+++ apps/ticket-trade/src/app/pages/concert/concert-detail/concert-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ticket-trade-concert-detail',
+  templateUrl: './concert-detail.component.html',
+  styleUrls: ['./concert-detail.component.css']
+})
+export class ConcertDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
