60e93de3abefa1d7bd7781cce47ce2298a94fd6c
Sybrand Bos <snjw.bos@student.avans.nl>
2024-10-22 22:56:00 +0200

    Domain models

---
+++ libs/domain/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/artist.interface';
+export * from './lib/concert.interface';
+export * from './lib/genre.interface';
+export * from './lib/location.interface';
+export * from './lib/ticket.interface';
+export * from './lib/user.interface';
---
+++ libs/domain/src/lib/location.interface.ts
@@ -0,0 +1,9 @@
+export interface ILocation {
+    id?: string;
+    street: string;
+    houseNumber: string;
+    postalcode: string;
+    country: string;
+    name: string;
+    city: string;
+}
