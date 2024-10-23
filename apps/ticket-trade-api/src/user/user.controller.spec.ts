7133b7700efa704266d01de84790ffcd935bff6f
Sybrand Bos <snjw.bos@student.avans.nl>
2024-10-23 15:52:01 +0200

    Fixed tests and github action

--- apps/ticket-trade-api/src/user/user.controller.spec.ts
+++ apps/ticket-trade-api/src/user/user.controller.spec.ts
@@ -1,14 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose'; 
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let mockUserModel = {
+    find: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getModelToken('User'), 
+          useValue: mockUserModel, 
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
@@ -17,4 +29,5 @@ describe('UserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
 });
--- apps/ticket-trade-api/src/user/user.service.spec.ts
+++ apps/ticket-trade-api/src/user/user.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';  // Dit wordt gebruikt om het Mongoose-model te mocken
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let mockUserModel = {
+    find: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getModelToken('User'),  // Mock het Mongoose model voor 'User'
+          useValue: mockUserModel,  // Gebruik het gemockte UserModel
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
