3c92a7d5416a3e0b134e9074e171dc346c264a1b
Sybrand Bos <snjw.bos@student.avans.nl>
2024-10-23 12:31:47 +0200

    Swagger documentation
    Added Swagger and fixed password validation pipe

--- apps/ticket-trade-api/src/user/dto/create-user.dto.ts
+++ apps/ticket-trade-api/src/user/dto/create-user.dto.ts
@@ -1,37 +1,48 @@
-import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString, Matches } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString, IsStrongPassword } from 'class-validator';
 import { TicketSchema } from '../../ticket/entities/ticket.entity';
 import { Prop } from '@nestjs/mongoose';
 import { ITicket, IUser } from '@ticket-trade/domain';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto implements IUser {
     
     // DB is responsible for ID
 
+    @ApiProperty({ example: 'John', description: 'Firstname of user' })
     @IsNotEmpty()
     @IsString()
     name: string;
 
+    @ApiProperty({ example: 'Doe', description: 'Lastname of user' })
     @IsNotEmpty()
     @IsString()
     lastname: string;
 
+    @ApiProperty({ example: 'johndoe', description: 'Username of user', uniqueItems: true })
     @IsNotEmpty()
     @IsString()
     username: string;
 
+    @ApiProperty({ example: '+31 6 12345678', description: 'Phonenumber of user' })
     @IsNotEmpty()
     @IsPhoneNumber(null, { message: 'Use the following phone number format : +31 6 12345678' })
     phone: string;
 
+    @ApiProperty({ example: 'john.doe@example.com', description: 'Emailadres of user' })
     @IsNotEmpty()
     @IsEmail()
     email: string;
-
+    
+    @ApiProperty({ example: 'Password@123', description: 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character' })
     @IsNotEmpty()
     @IsString()
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
-        message: 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
-    })
+    @IsStrongPassword({
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+      })
     password: string;
 
     @Prop({ type: [TicketSchema], default: [] }) // Automatically set to an empty array for tickets
--- apps/ticket-trade-api/src/user/user.controller.ts
+++ apps/ticket-trade-api/src/user/user.controller.ts
@@ -4,13 +4,18 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { ChangePasswordDto } from './dto/change-password.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
   @HttpCode(201)
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 201, description: 'User created successfully', type: User })
+  @ApiBody({ type: CreateUserDto })
   async create(@Body() createUserDto: CreateUserDto): Promise<Object> {
     const newUser = await this.userService.create(createUserDto);
     return {
@@ -21,17 +26,25 @@ export class UserController {
 
   @Get()
   @HttpCode(200)
+  @ApiOperation({ summary: 'Retrieve all users' })
+  @ApiResponse({ status: 200, description: 'List of all users', type: [User] })
   async findAll(): Promise<User[]> {
     return await this.userService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Retrieve single user by ID' })
+  @ApiResponse({ status: 200, description: 'Single user by ID', type: User })
   async findOne(@Param('id') id: string): Promise<User> {
     return await this.userService.findOne(id);
   }
 
   @Patch(':id')
   @HttpCode(200)
+  @ApiOperation({ summary: 'Update user by ID' })
+  @ApiResponse({ status: 201, description: 'User updated successfully', type: User })
+  @ApiBody({ type: UpdateUserDto })
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<Object> {
     const updatedUser = await this.userService.update(id, updateUserDto);
     return {
@@ -42,6 +55,9 @@ export class UserController {
 
   @Patch(':id/password')
   @HttpCode(200)
+  @ApiOperation({ summary: 'Update user password' })
+  @ApiResponse({ status: 201, description: 'Password changed successfully' })
+  @ApiBody({ type: ChangePasswordDto })
   async changePassword(@Param('id') id: string, @Body() changePasswordDto: ChangePasswordDto): Promise<Object> {
     await this.userService.changePassword(id, changePasswordDto);
     return {
@@ -51,6 +67,8 @@ export class UserController {
 
   @Delete(':id')
   @HttpCode(200)
+  @ApiOperation({ summary: 'Delete user by ID' })
+  @ApiResponse({ status: 201, description: 'User deleted successfully' })
   async remove(@Param('id') id: string): Promise<Object> {
     await this.userService.remove(id);
     return {
